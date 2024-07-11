665142dfbdf673dcbeb28eb0cb2e5f4b64d5eb3c
Sathyakartika2291 <sathyawinssathya@gmail.com>
2024-07-11 17:40:08 +0530

    outing modified

--- src/pages/Routing.js
+++ src/pages/Routing.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './Register/Register';
 import Login from './Login/Login';
 import Home from './Home/Home';
-import ServiceList from '../ServiceList/ServiceList';
+import ServiceList from './ServiceList/ServiceList';
 import Service from './Service/Service';
-import ServiceDetail from '../ServiceList/ServiceDetails';
+import ServiceDetail from './ServiceList/ServiceDetails';
 const Routing = () => {
     return (
         <div className="container">
